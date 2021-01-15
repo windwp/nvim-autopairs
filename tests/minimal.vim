ce1c55da8d912da723f78158fbc3ff6d37e3aab9
windwp <baronnight19@gmail.com>
2021-01-15 10:39:25 +0700

    add test WIP

--- tests/minimal.vim
+++ tests/minimal.vim
@@ -1,9 +1,7 @@
 set rtp +=.
-set rtp +=/home/trieu/.vim/autoload/plenary.nvim/
+set rtp +=~/.vim/autoload/plenary.nvim/
 runtime! plugin/plenary.vim
-source ~/.config/nvim/init.vim
 
 lua require("plenary/busted")
 lua require("nvim-autopairs")
-lua require("../tests/pairs")
 
--- tests/pairs_spec.lua
+++ tests/pairs_spec.lua
@@ -29,41 +29,29 @@ local npairs=require('nvim-autopairs')
 npairs.setup()
 local eq = assert.are.same
 
-function wait(time)
-    local duration = os.time() + time
-    while os.time() < duration do end
-end
 
 describe('autopairs ', function()
   for _, value in pairs(data) do
+    it("test "..value.name, function()
       local before = string.gsub(value.before , '%|' , "")
       local after = string.gsub(value.after , '%|' , "")
       local p_before = string.find(value.before , '%|')
       local p_after = string.find(value.after , '%|')
       local line = 1
-      -- add 1 space for simulate insert mode location
       vim.fn.setline(line , before)
       vim.fn.setpos('.' ,{0 , line , p_before , 0})
-      vim.fn.feedkeys("iaaa")
+      vim.fn.feedkeys("i")
       vim.fn.feedkeys(value.key)
-      vim.fn.feedkeys(npairs.esc("<ESC>"))
-      vim.cmd[[redraw]]
-      local done=false
-      local timer = vim.loop.new_timer()
-      timer:start(200, 0, vim.schedule_wrap(function()
-        done=true
-        local result = vim.fn.getline(line)
-        print('result'..vim.inspect(result))
-        local pos = vim.fn.getpos('.')
-        eq(p_after , pos[3]+ 1 , "\n\n pos error: " .. value.name .."\n")
-        eq(after, result , "\n\n text error: " .. value.name .."\n")
-        print("end")
-        return done
-      end))
-      vim.wait(1,function() return done end)
-      while done==false do end
+      vim.fn.feedkeys("<esc>")
+
+      -- I want to get result text after feedkeys excute my mapping
+      local result = vim.fn.getline(line)
+      local pos = vim.fn.getpos('.')
+      eq(p_after , pos[3]+ 1 , "\n\n pos error: " .. value.name .."\n")
+      eq(after, result , "\n\n text error: " .. value.name .."\n")
+
+    end)
   end
-  print("end")
   print("end")
 end)
 
