b12d4097a0bf37f52b90727724be770c0124eee8
windwp <baronnight19@gmail.com>
2021-01-17 09:03:06 +0700

    finish test

--- tests/pairs_spec.lua
+++ tests/pairs_spec.lua
@@ -1,33 +1,86 @@
-local data={
+local helpers = {}
+local npairs=require('nvim-autopairs')
+npairs.setup()
+local eq = assert.are.same
+_G.npairs = npairs;
+
+vim.api.nvim_set_keymap('i' , '<CR>','v:lua.npairs.check_break_line_char()', {expr = true , noremap = true})
+function helpers.feed(text, feed_opts)
+  feed_opts = feed_opts or 'n'
+  local to_feed = vim.api.nvim_replace_termcodes(text, true, false, true)
+  vim.api.nvim_feedkeys(to_feed, feed_opts, true)
+end
+
+function helpers.insert(text)
+  helpers.feed('i' .. text, 'x')
+end
+
+local data = {
   {
     name = "add normal bracket" ,
     key    = [[(]],
     before = [[aaaa| ]],
     after  = [[aaaa(|) ]]
   },
-   -- {
-   --  name = "add normal quote" ,
-   --  key    = [["]],
-   --  before = [[aa|aa]],
-   --  after  = [[aa"|"aa]]
-  -- },
-  -- {
-   --  name = "move right end line " ,
-   --  key    = [["]],
-   --  before = [[aaaa|"]],
-   --  after  = [[aaaa"|]]
-  -- },
-  -- {
-   --  name = "move right when inside quote" ,
-   --  key    = [["]],
-   --  before = [[("abcd|")]],
-   --  after  = [[("abcd"|())]]
-  -- }
-
+   {
+    name = "add normal quote" ,
+    key    = [["]],
+    before = [[aa| aa]],
+    after  = [[aa"|" aa]]
+  },
+   {
+    name = "don't add quote after alphabet char" ,
+    key    = [["]],
+    before = [[aa|aa]],
+    after  = [[aa"|aa]]
+  },
+  {
+    name = "move right end line " ,
+    key    = [["]],
+    before = [[aaaa|"]],
+    after  = [[aaaa"|]]
+  },
+  {
+    name = "move right when inside quote" ,
+    key    = [["]],
+    before = [[("abcd|")]],
+    after  = [[("abcd"|)]]
+  },
+  {
+    name = "move right when inside quote with special slash" ,
+    key    = [["]],
+    before = [[("abcd\"|")]],
+    after  = [[("abcd\""|)]]
+  },
+  {
+    name = "breakline on {" ,
+    filetype="javascript",
+    key    = [[<cr>]],
+    before = [[a{|}]],
+    after  = [[}]]
+  },
+  {
+    name = "breakline on (" ,
+    filetype="javascript",
+    key    = [[<cr>]],
+    before = [[a(|)]],
+    after  = [[)]]
+  },
+  {
+    name = "breakline on ]" ,
+    filetype="javascript",
+    key    = [[<cr>]],
+    before = [[a[|] ]],
+    after  = "] "
+  },
+  {
+    name = "breakline on < html" ,
+    filetype="html",
+    key    = [[<cr>]],
+    before = [[<div>|</div>]],
+    after  = [[</div>]]
+  }
 }
-local npairs=require('nvim-autopairs')
-npairs.setup()
-local eq = assert.are.same
 
 
 describe('autopairs ', function()
@@ -38,20 +91,31 @@ describe('autopairs ', function()
       local p_before = string.find(value.before , '%|')
       local p_after = string.find(value.after , '%|')
       local line = 1
+      if value.filetype ~= nil then
+        vim.bo.filetype = value.filetype
+      else
+        vim.bo.filetype = "text"
+      end
       vim.fn.setline(line , before)
       vim.fn.setpos('.' ,{0 , line , p_before , 0})
-      vim.fn.feedkeys("i")
-      vim.fn.feedkeys(value.key)
-      vim.fn.feedkeys("<esc>")
-
-      -- I want to get result text after feedkeys excute my mapping
+      helpers.insert(value.key)
+      helpers.feed("<esc>")
       local result = vim.fn.getline(line)
       local pos = vim.fn.getpos('.')
-      eq(p_after , pos[3]+ 1 , "\n\n pos error: " .. value.name .."\n")
-      eq(after, result , "\n\n text error: " .. value.name .."\n")
-
+      if value.key ~= '<cr>' then
+        eq(p_after , pos[3]+ 1 , "\n\n pos error: " .. value.name .. "\n")
+        eq(after, result , "\n\n text error: " .. value.name .. "\n")
+      else
+        local line2 = vim.fn.getline(line + 2)
+        eq(line + 1, pos[2], '\n\n breakline error:' .. value.name .. "\n")
+        eq(after, line2 , "\n\n text error: " .. value.name .. "\n")
+        vim.fn.setline(line, '')
+        vim.fn.setline(line+ 1, '')
+        vim.fn.setline(line+ 2, '')
+      end
     end)
   end
   print("end")
 end)
+
 
