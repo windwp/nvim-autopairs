0cacd33ec635430c80fd5522bad47662d3780f55
windwp <baronnight19@gmail.com>
2021-04-28 14:13:10 +0700

    feat: add more endwise rule

--- lua/nvim-autopairs/rules/endwise-lua.lua
+++ lua/nvim-autopairs/rules/endwise-lua.lua
@@ -1,7 +1,8 @@
 local endwise = require('nvim-autopairs.ts-rule').endwise
 
 local rules = {
-    endwise('then$', 'end', 'lua', 'if_statement')
+    endwise('then$', 'end', 'lua', 'if_statement'),
+    endwise('function.*%(.*%)$', 'end', 'lua', 'function_definition'),
 }
 
 
