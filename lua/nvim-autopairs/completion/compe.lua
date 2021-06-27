34ba10e9836781037aa308e2aac350f6da64f8e5
windwp <baronnight19@gmail.com>
2021-06-27 18:23:42 +0700

    fix(compe): check previous char is round bracket

--- lua/nvim-autopairs/completion/compe.lua
+++ lua/nvim-autopairs/completion/compe.lua
@@ -1,18 +1,24 @@
 local remap = vim.api.nvim_set_keymap
 local npairs = require('nvim-autopairs')
 local Completion = require('compe.completion')
+local utils = require('nvim-autopairs.utils')
 
 local method_kind = nil
 local function_kind = nil
 
 _G.MPairs.completion_done = function()
-    if method_kind == nil then
-        method_kind = require('vim.lsp.protocol').CompletionItemKind[2]
-        function_kind = require('vim.lsp.protocol').CompletionItemKind[3]
-    end
-    local item = Completion._confirm_item
-    if item.kind == method_kind or item.kind == function_kind then
-        vim.api.nvim_feedkeys('(', 'i', true)
+    local line = utils.text_get_current_line(0)
+    local _, col = utils.get_cursor()
+    local prev_char  = utils.text_cusor_line(line, col, 1, 1, false)
+    if prev_char ~= "(" then
+        if method_kind == nil then
+            method_kind = require('vim.lsp.protocol').CompletionItemKind[2]
+            function_kind = require('vim.lsp.protocol').CompletionItemKind[3]
+        end
+        local item = Completion._confirm_item
+        if item.kind == method_kind or item.kind == function_kind then
+            vim.api.nvim_feedkeys('(', 'i', true)
+        end
     end
 end
 
