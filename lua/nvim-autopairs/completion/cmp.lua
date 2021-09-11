7f905cb5b705d483f4e8bf56eba86dcafe788905
JezerM <amyuki4@icloud.com>
2021-09-10 18:29:13 -0600

    Added map_char option to modify map_complete auto-insertion

--- lua/nvim-autopairs/completion/cmp.lua
+++ lua/nvim-autopairs/completion/cmp.lua
@@ -4,9 +4,16 @@ local cmp = require('cmp')
 
 local M = {}
 M.setup = function(opt)
-    opt = opt or { map_cr = true, map_complete = true, auto_select = true }
+    opt = opt or { map_cr = true, map_complete = true, auto_select = true, map_char = {all = '('} }
+    if not opt.map_char then
+        opt.map_char = {all = '('}
+    end
+    if opt.map_char["all"] == nil then
+        opt.map_char["all"] = '('
+    end
     local map_cr = opt.map_cr
     local map_complete = opt.map_complete
+    local map_char = opt.map_char
     local cmp_setup = {}
     if map_cr then
         cmp_setup.mapping = {
@@ -25,13 +32,18 @@ M.setup = function(opt)
     if map_complete then
         local method_kind = cmp.lsp.CompletionItemKind.Method
         local function_kind = cmp.lsp.CompletionItemKind.Function
+        local filetype = vim.fn.expand("%:e")
+        if map_char[filetype] == nil then
+            filetype = "all"
+        end
+        local char = map_char[filetype]
         cmp_setup.event = {
             on_confirm_done = function(entry)
                 local line = utils.text_get_current_line(0)
                 local _, col = utils.get_cursor()
                 local prev_char, next_char = utils.text_cusor_line(line, col, 1, 1, false)
                 local item = entry:get_completion_item()
-                if prev_char ~= '(' and next_char ~= '(' then
+                if prev_char ~= char and next_char ~= char then
                     if item.kind == method_kind or item.kind == function_kind then
                         -- check insert text have ( from snippet
                         if
@@ -44,7 +56,7 @@ M.setup = function(opt)
                         then
                             return
                         end
-                        vim.api.nvim_feedkeys('(', '', true)
+                        vim.api.nvim_feedkeys(char, '', true)
                     end
                 end
             end,
