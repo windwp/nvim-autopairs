238672724d58ac554c73074da938462c304c6968
Sam-programs <None>
2023-10-14 13:31:21 +0300

    made the function call return the function

--- lua/nvim-autopairs/completion/cmp.lua
+++ lua/nvim-autopairs/completion/cmp.lua
@@ -1,7 +1,6 @@
 local autopairs = require('nvim-autopairs')
 local handlers = require('nvim-autopairs.completion.handlers')
 local cmp = require('cmp')
-local utils = require('nvim-autopairs.utils')
 
 local Kind = cmp.lsp.CompletionItemKind
 
@@ -53,7 +52,7 @@ M.on_confirm_done = function(opts)
         if evt.commit_character then
           return
         end
-
+        
         local entry = evt.entry
         local commit_character = entry:get_commit_characters()
         local bufnr = vim.api.nvim_get_current_buf()
@@ -84,59 +83,61 @@ M.on_confirm_done = function(opts)
     end
 end
 
-M.cpp_pairs = function(evt)
-    if not (vim.o.filetype == "c" or vim.o.filetype == "cpp") then
-        return
-    end
+M.cpp_pairs = function()
+    local cmp_config = require('cmp.config')
+    local cmp_comparetors = cmp_config.get().sorting.comparators
 
-    local c = vim.api.nvim_win_get_cursor(0)[2]
-    local line = vim.api.nvim_get_current_line()
-    if line:sub(c, c) ~= '>' then
-        return
-    end
-
-    local entry = evt.entry
-    local item = entry:get_completion_item()
-    local pairs = ''
-    local functionsig = item.label
-    if  (functionsig:sub(#functionsig, #functionsig) == '>' or
-            functionsig == ' template')
-    then
-        if functionsig:sub(2, 8) == 'include' then
-            pairs = ' '
+    local unpack = unpack or table.unpack
+    local function cpp_sort_cmp(entry1, entry2)
+        local kind1 = entry1.completion_item.kind
+        local kind2 = entry2.completion_item.kind
+        if vim.o.filetype ~= "cpp" then
+            return nil
         end
-        pairs = pairs .. '<>'
-        pairs = pairs .. utils.esc('<left>')
-        vim.api.nvim_feedkeys(pairs, "n", false)
+        if kind1 == Kind.Constructor and kind2 == Kind.Class then
+            return false
+        end
+        if kind1 == Kind.Class and kind2 == Kind.Constructor then
+            return true
+        end
+        return nil
+    end
+
+    cmp.setup({
+        sorting = {
+            comparators = {
+                cpp_sort_cmp,
+                unpack(cmp_comparetors),
+            }
+        }
+    })
+    return function(evt)
+        if not (vim.o.filetype == "c" or vim.o.filetype == "cpp") then
+            return
+        end
+
+        local c = vim.api.nvim_win_get_cursor(0)[2]
+        local line = vim.api.nvim_get_current_line()
+        if line:sub(c, c) == '>' then
+            return
+        end
+
+        local entry = evt.entry
+        local item = entry:get_completion_item()
+        local pairs = ''
+        local functionsig = item.label
+        if (functionsig:sub(#functionsig, #functionsig) == '>' or
+                functionsig == ' template')
+        then
+            if functionsig:sub(2, 8) == 'include' then
+                pairs = ' '
+            end
+            pairs = pairs .. '<>'
+            pairs = pairs .. utils.esc('<left>')
+            vim.api.nvim_feedkeys(pairs, "n", false)
+        end
     end
 end
 
-local cmp_config = require('cmp.config')
-local cmp_comparetors = cmp_config.get().sorting.comparators
-
-local unpack = unpack or table.unpack
-local function cpp_sort_cmp(entry1, entry2)
-   local kind1 = entry1.completion_item.kind
-   local kind2 = entry2.completion_item.kind
-   if vim.o.filetype ~= "cpp" then
-      return nil
-   end
-   if kind1 == Kind.Constructor and kind2 == Kind.Class then
-      return false
-   end
-   if kind1 == Kind.Class and kind2 == Kind.Constructor then
-      return true
-   end
-   return nil
-end
-
-cmp.setup({
-   sorting = {
-      comparators = {
-         cpp_sort_cmp,
-         unpack(cmp_comparetors),
-      }
-   }
-})
 
 return M
