ba2bd2b138c3b6821023fac7337eed49e7140464
Kamal Sacranie <kamalsjunkmail@gmail.com>
2024-01-19 17:01:55 +0000

    feat: allow rules to be treesitter context aware
    When a rule is defined with the `:with_context()` method, has a
    specified filetype, and is operating in a buffer with a treesitter
    parser attached, the rule will only execute iff the treesitter language
    at the cursor matches one of the filetypes specified in the initial rule
    definition.
    
    > If there are no specified filetypes, of there is no parser attached to
    > the current buffer, the rule executes as normal

--- lua/nvim-autopairs/ts-utils.lua
+++ lua/nvim-autopairs/ts-utils.lua
@@ -1,6 +1,18 @@
 local ts_get_node_text = vim.treesitter.get_node_text or vim.treesitter.query.get_node_text
 local M = {}
 
+--- Returns the language tree at the given position.
+---@return LanguageTree
+function M.get_language_tree_at_position(position)
+    local language_tree = vim.treesitter.get_parser()
+    language_tree:for_each_tree(function(_, tree)
+        if tree:contains(vim.tbl_flatten({ position, position })) then
+            language_tree = tree
+        end
+    end)
+    return language_tree
+end
+
 function M.get_tag_name(node)
   local tag_name = nil
   if node ~=nil then
