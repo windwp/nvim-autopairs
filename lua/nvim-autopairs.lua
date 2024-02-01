331d562582d75f7ff5d5e9d9e06b0e29700623b0
JosefLitos <litosjos@fit.cvut.cz>
2024-02-01 16:20:16 +0100

    fix: avoid global variable usage

--- lua/nvim-autopairs.lua
+++ lua/nvim-autopairs.lua
@@ -311,8 +311,8 @@ M.on_attach = function(bufnr)
             bufnr,
             'i',
             '<bs>',
-            'v:lua.MPairs.autopairs_bs()',
-            { expr = true, noremap = true }
+            '',
+            { callback = M.autopairs_bs, expr = true, noremap = true }
         )
     end
 
@@ -321,8 +321,8 @@ M.on_attach = function(bufnr)
             bufnr,
             "i",
             utils.key.c_h,
-            'v:lua.MPairs.autopairs_c_h()',
-            { expr = true, noremap = true }
+            '',
+            { callback = M.autopairs_c_h, expr = true, noremap = true }
         )
     end
 
@@ -331,8 +331,8 @@ M.on_attach = function(bufnr)
             bufnr,
             'i',
             '<c-w>',
-            'v:lua.MPairs.autopairs_c_w()',
-            { expr = true, noremap = true }
+            '',
+            { callback = M.autopairs_c_w, expr = true, noremap = true }
         )
     end
     api.nvim_buf_set_var(bufnr, 'nvim-autopairs', 1)
@@ -654,11 +654,10 @@ M.map_cr = function()
     api.nvim_set_keymap(
         'i',
         '<CR>',
-        'v:lua.MPairs.completion_confirm()',
-        { expr = true, noremap = true }
+        '',
+        { callback = M.completion_confirm, expr = true, noremap = true }
     )
 end
 
 M.esc = utils.esc
-_G.MPairs = M
 return M
