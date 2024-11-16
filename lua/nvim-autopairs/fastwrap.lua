e931f415213abf851694f090009eae9ffc620dc8
Zeioth <Zeioth@hotmail.com>
2024-11-16 22:21:49 +0100

    ♻️refactor(vim.hl): More elegant solution.

--- lua/nvim-autopairs/fastwrap.lua
+++ lua/nvim-autopairs/fastwrap.lua
@@ -215,15 +215,7 @@ M.highlight_wrap = function(tbl_pos, row, col, end_col, whitespace_line)
         })
     else
         if config.highlight_grey then
-            -- TODO: Deleteme after nvim 0.11 is released.
-            local hl
-            vim.fn.has("nvim-0.11") == 1 then
-                hl = vim.hl
-            else
-                hl = vim.highlight
-            end
-    
-            hl.range(
+            (vim.hl or vim.highlight).range(
                 bufnr,
                 M.ns_fast_wrap,
                 config.highlight_grey,
